9fc64df52739448d40e515a0ef6edfa230d585e6
rlem0n <89490145+rlem0n@users.noreply.github.com>
2021-09-04 09:44:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1355,7 +1355,7 @@ Rect.prototype.contains = function(x, y) {
 ////////////////////////////////////////////////////////////////
 
 	var channel_id = decodeURIComponent(window.location.hash.substr(1)) || "lobby";
-	var gClient = new Client("wss://20.85.232.79:1234");
+	var gClient = new Client("wss://multiplayerpianoserver.hellogame.repl.co");
 	gClient.setChannel(channel_id);
 	gClient.start();
 
